b424963baf4c7f8631152ed9e8340e479ad5fe74
Scott Bewick <bewick.scott@gmail.com>
2016-10-24 00:21:59 -0400

    added Story class, scraped polls page, added scrape_polls method, which lists all polls

--- lib/poll_tracker/scraper.rb
+++ lib/poll_tracker/scraper.rb
@@ -1,6 +1,55 @@
 require 'pry'
-class PollTracker::Scaper
+require 'nokogiri'
+require 'open-uri'
+
+class PollTracker::Scraper
 
 
+
+  def get_page 
+    doc = Nokogiri::HTML(open("http://elections.huffingtonpost.com/pollster/2016-general-election-trump-vs-clinton"))  
+  end
+
+  def get_page_2
+    doc_2 = Nokogiri::HTML(open("http://elections.huffingtonpost.com/pollster/polls/abc-news-26379"))  
+    binding.pry
+  end  
+
+  def scrape_polls
+    names = []
+    poll_names = get_page.css("div.scrollable-poll-table table#poll-table tbody td.poll div.pollster a")
+    poll_names.children.collect do |name|
+      names << name.text
+    end
+    names 
+    binding.pry
+  end  
 
 end  
+
+  
+  # Huffpost model - poll of polls - 326 pols from 42 pollsters
+  # Clinton name - doc.css("ul#chart-choice-select li label.checked span.choice").first.text
+  # Clinton number - doc.css("ul#chart-choice-select li label.checked span.value").first.text.gsub("%", "").to_f
+  # Trump name - doc.css("ul#chart-choice-select li label.checked span.choice").last.text
+  # Trump number - doc.css("ul#chart-choice-select li label.checked span.value").last.text.gsub("%", "").to_f     
+
+  #Polls
+  # candidates names(including "other", "undecided" and "spread") iterate w/ #each
+  # doc.css("div#pollster-polls div.scrollable-poll-table table#poll-table").first
+  
+  # doc.css("div.scrollable-poll-table table#poll-table tr th.choice").each_with_index do |cand, i|
+  #    puts "#{i+1}. #{cand.text}"
+  # end
+  # STDOUT the following
+  # 1. Trump
+  # 2. Clinton
+  # 3. Other
+  # 4. Undecided 
+
+  #candidates = doc.css("div.scrollable-poll-table table#poll-table tr th.choice").text.split(/([[:upper:]][[:lower:]]*)/).delete_if(&:empty?)
+  # this will return the following array:
+  # ["Trump", "Clinton", "Other", "Undecided"]
+
+  # doc.css("div.scrollable-poll-table table#poll-table tr th.spread").text
+  # returns the word 'Spread' number 5 on the list above
---
+++ lib/poll_tracker/story.rb
@@ -0,0 +1,6 @@
+class PollTracker::Story
+
+
+
+
+end
